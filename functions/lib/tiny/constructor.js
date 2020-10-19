25736bb4ece430e4251c228de11baae88b660c43
Leonardo Matos <leomp120894@gmail.com>
2020-10-19 01:33:20 -0300

    refactor(tiny-client): expose 'post' method, accept string/number params

--- functions/lib/tiny/constructor.js
+++ functions/lib/tiny/constructor.js
@@ -2,21 +2,30 @@ const axios = require('axios')
 const xmlJs = require('xml-js')
 
 module.exports = function (token) {
-  this.request = options => {
+  this.post = (url, body, options) => {
     // https://www.tiny.com.br/ajuda/api/api2
     let data = `token=${token}&formato=JSON`
-    if (options.data) {
-      for (const field in options.data) {
-        if (typeof options.data[field] === 'object') {
-          data += `&${field}=${xmlJs.js2xml(options.data[field])}`
+    if (body) {
+      for (const field in body) {
+        if (body[field]) {
+          switch (typeof body[field]) {
+            case 'object':
+              data += `&${field}=${xmlJs.js2xml(body[field])}`
+              break
+            case 'string':
+            case 'number':
+              data += `&${field}=${body[field]}`
+          }
         }
       }
     }
-    return axios({
-      ...options,
+
+    return axios.post(url, data, {
       baseURL: 'https://api.tiny.com.br/api2/',
-      data
+      timeout: 30000,
+      ...options
     })
   }
+
   return this
 }
