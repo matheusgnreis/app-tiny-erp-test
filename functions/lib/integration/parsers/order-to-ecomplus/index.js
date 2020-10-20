9ce2bc5fecdc7cf446400c1590d10ce17e1369b6
Leonardo Matos <leomp120894@gmail.com>
2020-10-20 18:38:18 -0300

    chore(import-order): remove debugs

--- functions/lib/integration/parsers/order-to-ecomplus/index.js
+++ functions/lib/integration/parsers/order-to-ecomplus/index.js
@@ -1,15 +1,11 @@
 module.exports = (tinyOrder, shippingLines, tiny) => new Promise((resolve, reject) => {
-  console.log(JSON.stringify(tinyOrder))
   const partialOrder = {}
   if (tinyOrder.obs_interna) {
     partialOrder.staff_notes = tinyOrder.obs_interna
   }
 
-  console.log(JSON.stringify(shippingLines))
-
   if (shippingLines && shippingLines.length) {
     const shippingLine = shippingLines[0]
-    console.log(JSON.stringify(shippingLine))
     if (
       tinyOrder.codigo_rastreamento &&
       (!shippingLine.tracking_codes || !shippingLine.tracking_codes.length)
@@ -17,18 +13,14 @@ module.exports = (tinyOrder, shippingLines, tiny) => new Promise((resolve, rejec
       const tracking = {
         code: tinyOrder.codigo_rastreamento
       }
-      console.log(JSON.stringify(tracking))
       if (tinyOrder.url_rastreamento) {
         tracking.link = tinyOrder.url_rastreamento
       }
       shippingLine.tracking_codes = [tracking]
-      console.log(JSON.stringify(shippingLine))
       partialOrder.shipping_lines = shippingLines
     }
-    console.log(JSON.stringify(partialOrder))
 
     if (tinyOrder.id_nota_fiscal > 0) {
-      console.log(2, JSON.stringify(partialOrder))
       shippingLine.invoices = []
       return tiny.post('/nota.fiscal.obter.php', { id: tinyOrder.id_nota_fiscal })
         .then(tinyInvoice => {
@@ -39,7 +31,6 @@ module.exports = (tinyOrder, shippingLines, tiny) => new Promise((resolve, rejec
               serial_number: String(tinyInvoice.nota_fiscal.serie)
             })
           }
-          console.log(3)
           partialOrder.shipping_lines = shippingLines
           resolve(partialOrder)
         })
@@ -47,6 +38,5 @@ module.exports = (tinyOrder, shippingLines, tiny) => new Promise((resolve, rejec
     }
   }
 
-  console.log(JSON.stringify(partialOrder))
   resolve(partialOrder)
 })
