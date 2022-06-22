7132cf1583b7e6ab7c82c18d15d26fc9456277b0
Leonardo Matos <leomp120894@gmail.com>
2022-06-22 18:13:22 -0300

    Update functions/__env.js
    [skip ci]

--- functions/__env.js
+++ functions/__env.js
@@ -18,7 +18,7 @@ const functionName = server && server.functionName ? server.functionName : 'app'
 module.exports = {
   functionName,
   operatorToken: server && server.operator_token,
-  baseUri: process.env.SERVER_BASE_URI ||
+  baseUri: (server && server.base_uri) ||
     `https://us-central1-${process.env.GCLOUD_PROJECT}.cloudfunctions.net/${functionName}`,
   pkg: {
     ...pkg
