2204ac276ab60156072ad1cd26a992bbcfdb5c39
Leonardo Matos <leomp120894@gmail.com>
2023-03-10 13:37:24 -0300

    fix(clear): properly delete old stock entries by timestamp

--- functions/lib/integration/clear-tiny-states.js
+++ functions/lib/integration/clear-tiny-states.js
@@ -3,6 +3,7 @@ const { firestore } = require('firebase-admin')
 module.exports = context => {
   const date = new Date()
   date.setDate(date.getDate() - 1)
+  const timestamp = firestore.Timestamp.fromDate(date)
 
   const delOldDocs = (collName, fieldName, fieldVal) => {
     return firestore()
@@ -11,22 +12,24 @@ module.exports = context => {
       .limit(500)
       .get().then(async querySnapshot => {
         for (const doc of querySnapshot.docs) {
+          // eslint-disable-next-line no-await-in-loop
           await doc.ref.delete()
         }
         return querySnapshot
       })
   }
 
-  return delOldDocs('tiny_orders', 'updatedAt', firestore.Timestamp.fromDate(date))
+  return delOldDocs('tiny_orders', 'updatedAt', timestamp)
     .then(querySnapshot => {
       console.log(`> Deleted ${querySnapshot.size} Tiny order states`)
       return delOldDocs('integration_retries', 'd', date.toISOString())
     })
     .then(querySnapshot => {
       console.log(`> Deleted ${querySnapshot.size} integration retries`)
-      return delOldDocs('tiny_stock_updates', 'updatedAt', date.toISOString())
+      return delOldDocs('tiny_stock_updates', 'updatedAt', timestamp)
     })
     .then(querySnapshot => {
       console.log(`> Deleted ${querySnapshot.size} Tiny stock entries`)
+      return true
     })
 }
