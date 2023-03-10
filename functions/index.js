9e5cb48998c8504c1ec072778d5aa613e3da84dc
Leonardo Matos <leomp120894@gmail.com>
2023-03-10 13:50:17 -0300

    chore: edit clear states function cron

--- functions/index.js
+++ functions/index.js
@@ -161,7 +161,7 @@ console.log(`-- Sheduled active sync from Tiny API '${syncCron}'`)
 
 // delete old stored Tiny order states
 const clearTinyStates = require('./lib/integration/clear-tiny-states')
-const clearStatesCron = '10 17 * * *'
+const clearStatesCron = '16 59 * * *'
 exports.scheduledClear = functions.pubsub.schedule(clearStatesCron).onRun(clearTinyStates)
 console.log(`-- Sheduled clearing Tiny stored states '${clearStatesCron}'`)
 
