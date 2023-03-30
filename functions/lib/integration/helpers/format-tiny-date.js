6913d7092f04a4d97ef25d833e2020b42b8330b3
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-03-29 22:58:49 -0300

    fix(format-date): set less 3 hours from zero utc server date

--- functions/lib/integration/helpers/format-tiny-date.js
+++ functions/lib/integration/helpers/format-tiny-date.js
@@ -1,4 +1,5 @@
-module.exports = d => {
+module.exports = date => {
+  const d = new Date(date.getTime() - (3 * 60 * 60 * 1000))
   return d.getDate().toString().padStart(2, '0') + '/' +
     (d.getMonth() + 1).toString().padStart(2, '0') + '/' +
     d.getFullYear()
