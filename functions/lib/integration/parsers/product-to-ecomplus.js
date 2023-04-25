8f640316a9c653a011b60f4067932a471f94e8b3
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-04-25 15:08:57 -0300

    chore: update product slug from tiny

--- functions/lib/integration/parsers/product-to-ecomplus.js
+++ functions/lib/integration/parsers/product-to-ecomplus.js
@@ -93,11 +93,15 @@ module.exports = (tinyProduct, storeId, auth, isNew = true, tipo) => new Promise
   }
 
   if (isNew) {
-    product.slug = removeAccents(name.toLowerCase())
+    if (tinyProduct.slug) {
+      product.slug = tinyProduct.slug
+    } else {
+      product.slug = removeAccents(name.toLowerCase())
       .replace(/\s+/g, '-')
       .replace(/[^a-z0-9-_./]/g, '')
-    if (!/[a-z0-9]/.test(product.slug.charAt(0))) {
-      product.slug = `p-${product.slug}`
+      if (!/[a-z0-9]/.test(product.slug.charAt(0))) {
+        product.slug = `p-${product.slug}`
+      }
     }
   }
 
